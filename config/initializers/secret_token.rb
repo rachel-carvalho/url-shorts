593b45940b48f01b6b1dda90ca5d687422f89e4a
Rachel Carvalho <rachel.carvalho@gmail.com>
2014-01-02 20:50:21 -0500

    base rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UrlShorts::Application.config.secret_key_base = 'd43ff9fdab11bbf3bc460196cb1ba1df5dfcae507c18893ce6eec252e5c264c5d5cfd879d19fc99cbc7d5a48daf9b6cef80edb0ed13bcdba7af710c86c0d864b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UrlShorts::Application.config.session_store :cookie_store, key: '_url-shorts_session'
