340ac93a849d2749d3198a55d980d23aeb836dd7
Rachel Carvalho <rachel.carvalho@gmail.com>
2014-01-02 22:07:16 -0500

    stubbed to and from short url

--- app/models/url.rb
+++ app/models/url.rb
@@ -1,5 +1,12 @@
 class Url
   include Mongoid::Document
   field :original, type: String
-  field :short, type: String
+
+  def short
+    _id
+  end
+
+  def self.id_from_short(s)
+    s
+  end
 end
